7917f9c524a6c991a72397a36ac776b839c68c11
terious78 <taesuk.moon@gmail.com>
2016-10-18 15:12:04 -0400

    Ex.4

--- exercise4.rb
+++ exercise4.rb
@@ -1,1 +1,16 @@
+#FizzBuzz
 
+hundred = (1..100)
+
+hundred.each do |num|
+
+  if num%3 == 0 && num%5 == 0
+    puts "BitMaker"
+  elsif num%3 == 0
+    puts "Bit"
+  elsif num%5 == 0
+    puts "Maker"
+  else
+    puts num
+  end
+end
